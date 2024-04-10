a21802a1c634517a38332e0d1ca574c4ced0fd91
Drew Hughlett <drewrh04@gmail.com>
2024-04-10 23:48:10 +0000

    ♻️ Remove .js files

--- src/index.js
+++
@@ -1,5 +0,0 @@
-// import getAllPlaylists from './lib/playlists/get-all-playlists.js'
-// import getLikedTracks from './lib/tracks/get-liked-tracks.js'
-// import getPlaylistTracks from './lib/tracks/get-playlist-tracks.js'
-// const tracks = await getLikedTracks()
-console.log('Hello')
