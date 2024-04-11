1ec584086a2482fcf5952a44e2b1efe93e6eb4fc
Drew Hughlett <drewrh04@gmail.com>
2024-04-11 03:20:10 +0000

    🚧 Testing user experience

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ import getLikedTracks from './lib/tracks/get-liked-tracks.js'
 import getPlaylistTracks from './lib/tracks/get-playlist-tracks.js'
 
 const tracks = await getLikedTracks()
+
+export const playlists = {
+  playlists: [
+    { artists: ['Mac Miller'] },
+    { artists: ['A$AP Rocky'] },
+    { artists: ['Gorillaz'] },
+    { artists: ['21 Savage'] },
+    { artists: ['Chance the Rapper'] },
+    { artists: ['UGK', 'Pimp C', 'Bun B'], name: 'UGK' },
+  ],
+}
