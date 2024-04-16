a745f0dff370bc8966cf16b73327a28ebb0a0ef5
Drew Hughlett <drewrh04@gmail.com>
2024-04-15 22:03:01 -0400

    🚧 Test workflow

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import { managedPlaylists } from '../data/managedPlaylists.js'
 import { login } from './lib/login/login.js'
 import { createAPI } from './lib/spotify-api/create-api.js'
 
-await createAPI()
+const spotify = await createAPI()
+
+const user = spotify.currentUser.profile.name
+
+console.log(user)
 
 // await processManagedPlaylists(managedPlaylists)
 // await processDanglingTracks()
