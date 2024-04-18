7f29a1ed76ceac31277166bea192e65dca0bc127
Drew Hughlett <drewrh04@gmail.com>
2024-04-17 20:34:09 -0400

    ♻️ Refactor

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@ import { managedPlaylists } from '../data/managedPlaylists.js'
 import { processDanglingTracks } from './lib/playlists/process-dangling-tracks.js'
 import { processManagedPlaylists } from './lib/playlists/process-managed-playlist.js'
 
-async function run() {
-  await processManagedPlaylists(managedPlaylists)
-  await processDanglingTracks()
-}
-
-await run()
+await processManagedPlaylists(managedPlaylists)
+await processDanglingTracks()
