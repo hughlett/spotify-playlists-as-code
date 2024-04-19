12eea382971da9bec806a2d677c45d01561dd914
Drew Hughlett <drewrh04@gmail.com>
2024-04-19 19:05:19 +0000

    🐛 Fix error being thrown without reason

--- commands/process-managed-playlists/index.ts
+++ commands/process-managed-playlists/index.ts
@@ -2,7 +2,7 @@ import { existsSync } from 'fs'
 import run from './run.js'
 
 if (
-  !existsSync('/data/managedPlaylists.ts') ||
+  !existsSync('/data/managedPlaylists.ts') &&
   !existsSync('../../data/managedPlaylists.ts')
 ) {
   throw new Error('No managed playlists!')
