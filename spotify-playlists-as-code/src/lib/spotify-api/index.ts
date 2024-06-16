61cfa7953c64f3e2fe2d1cd0622f7b897c6a88b7
Drew Hughlett <drewrh04@gmail.com>
2024-06-16 13:49:09 -0400

    ♻️ Remove tokens folder

--- spotify-playlists-as-code/src/lib/spotify-api/index.ts
+++ spotify-playlists-as-code/src/lib/spotify-api/index.ts
@@ -6,8 +6,7 @@ import {
 } from '@spotify/web-api-ts-sdk'
 import getUserLikedTracks from '../tracks/get-user-liked-tracks.js'
 
-export const REFRESH_TOKEN_PATH =
-  '/spotify-playlists-as-code/data/tokens/refresh_token'
+export const REFRESH_TOKEN_PATH = '/token'
 export const CLIENT_ID = process.env.CLIENT_ID || ''
 
 /**
