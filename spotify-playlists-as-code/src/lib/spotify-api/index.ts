970d47319dc7e156e242368c3a1dd03f4d08b9cb
Drew Hughlett <drewrh04@gmail.com>
2024-10-13 12:31:07 -0400

    ♻️ Change refresh token directory

--- spotify-playlists-as-code/src/lib/spotify-api/index.ts
+++ spotify-playlists-as-code/src/lib/spotify-api/index.ts
@@ -6,7 +6,8 @@ import {
 } from '@spotify/web-api-ts-sdk'
 import getUserLikedTracks from '../tracks/get-user-liked-tracks.js'
 
-export const REFRESH_TOKEN_PATH = '/token'
+export const REFRESH_TOKEN_PATH =
+  '/spotify-playlists-as-code/data/refresh_token'
 export const CLIENT_ID = process.env.CLIENT_ID || ''
 
 /**
